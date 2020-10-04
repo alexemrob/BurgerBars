3014b9366a1bd7c2ed5a20f11e640b7b1c9573a5
Alexandria Robertson <alexemrob@gmail.com>
2020-10-04 10:45:40 -0500

    create burger is functional

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -7,32 +7,33 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 // Default the route to home page
-router.get('/',function(req,res){
+router.get('/', function (req, res) {
     res.redirect("/burgers");
 });
 // Get burgers
-router.get('/burgers',function(req,res){
-    burger.select(function(data){
+router.get('/burgers', function (req, res) {
+    burger.select(function (data) {
         var hbsObject = { burgers: data };
-        res.render('index',hbsObject);
+        res.render('index', hbsObject);
     });
 });
 //Add burger
-router.post("/burgers/create", function (req, res) {
+router.post('/burgers/create', function (req, res) {
     burger.create(["burger_name"], [req.body.burger_name], function (result) {
         res.redirect("/burgers");
     });
 });
 //devour burger - update
-router.put("/burgers/update/:id", function (req, res) {
-    var condition = "id = ${req.params.id}";
+router.put('/burgers/update/:id', function (req, res) {
+    var condition = 'id = ${req.params.id}';
 
     burger.update(
         {
-            'devoured': req.body.devoured },
+            'devoured': req.body.devoured
+        },
         condition,
         function (data) {
-           res.redirect('/burgers');
+            res.redirect('/burgers');
 
         }
     );
